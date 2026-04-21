d08db0d0345b97d8aeb2945120e03dab219a7ff8
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-04-21 09:43:57 +0500

    fector

--- loopsQ.js
+++ loopsQ.js
@@ -1,9 +1,44 @@
-const usernumber = +prompt("Elease Enter Number");
+const usernumber = +prompt("Please Enter Number");
 // for(let i= usernumber; i > 0; i-- ){
 // console.log(i);
 // }
 
-for(let i = 1 ; i<=10 ; i++){
-console.log(`${usernumber}*${i}=${usernumber*i}`);
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${usernumber}*${i}=${usernumber * i}`);
+// }
+
+// let sum = 0;
+// for(let i = 1 ; i<=usernumber;i++){
+// sum+=i
+// }
+// console.log(sum);
+
+// let factorial  = 1;
+// for(let i = 1 ; i<=usernumber;i++){
+// factorial*=i
+// }
+// console.log(factorial);
+
+// const a = +prompt("Please Enter Start Number");
+// const b = +prompt("Please Enter End Number");
+
+// let sumOfEven = 0;
+// let sumOfOdd = 0;
+
+// for (let i = a; i <= b; i++) {
+//   if (i % 2 === 0) {
+//     sumOfEven += i;
+//   } else {
+//     sumOfOdd += i;
+//   }
+// }
+// console.log(sumOfEven);
+// console.log(sumOfOdd);
+
+for(let i = 2 ; i<= Math.trunc(usernumber/2) ; i++){
+if(usernumber%i===0){
+    console.log(i);
 }
+}
+
 
